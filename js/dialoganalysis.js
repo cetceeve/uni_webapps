8384c0c722af7b3df91c65cc094e5b4cae89c2e6
Fabian Zeiher <fzeiher@gmail.com>
2018-09-04 23:04:24 +0200

    FEAT: dialog with analysis

---
+++ js/dialoganalysis.js
@@ -0,0 +1,47 @@
+var mText;
+
+function analyseText(text) {
+  mText = text;
+  checkLength();
+  checkChars();
+  checkWords();
+  checkSpecials();
+}
+
+// Länge des eingegebenen Textes
+function checkLength() {
+  document.getElementById('textlength').value = mText.length;
+}
+
+// Anzahl der unterschiedlichen Zeichen (ohne Leerzeichen)
+function checkChars() {
+  let chars = [];
+  let text = mText.replace(/ /g, "");
+  for (let i = 0; i < text.length; i++) {
+    if (!contains(text.charAt(i), chars)) {
+      chars.push(text.charAt(i));
+    }
+  }
+  document.getElementById('chars').value = chars.length;
+}
+
+function contains(c, text) {
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === c) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Anzahl der Wörter
+function checkWords() {
+  document.getElementById('words').value = mText.split(" ").length;
+}
+
+
+// Anzahl der Sonderzeichen und Zahlen (bzw. Nichtbuchstaben ( a-z , A-Z ))
+function checkSpecials() {
+  let specials = mText.replace(/[a-zA-Z]/g, "");
+  document.getElementById('special').value = specials.length;
+}
